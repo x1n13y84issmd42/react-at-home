0d789c8cc3b4e3059c955a65258b0c19a01e2499
x1n13y84issmd42 <x1n13y84issmd42@gmail.com>
2026-03-29 12:06:29 +0300

    Less pointless logging

--- src/state.ts
+++ src/state.ts
@@ -59,7 +59,7 @@ export namespace State {
 
 		//TODO: this proxy usage needs to be revised both design- & performance-wise.
 		function get(o: T, p: Keys<T> | 'hooks'): any {
-			ctx && console.log(`Context#${ctx.name}`);
+			// ctx && console.log(`Context#${ctx.name}`);
 
 			if (p === 'hooks') {
 				return hooks;
@@ -67,7 +67,7 @@ export namespace State {
 				stash[p] = stash[p] || new Set<Context>();
 				hooks.get.call(stash[p]);
 				const res = o[p];
-				console.log(p, '=', res);
+				// console.log(p, '=', res);
 				return res;
 			}
 		}
@@ -84,8 +84,10 @@ export namespace State {
 		return new Proxy<T>(
 			{...state},
 			{
-				get: LogGroup.wrap(get, `[] Proxy get()`, (_, p) => p, {collapsed: true}),
-				set: LogGroup.wrap(set, `[] Proxy set()`, (_, p, v) => `${p}=${v}`, {collapsed: true}),
+				// get: LogGroup.wrap(get, `[] Proxy get()`, (_, p) => p, {collapsed: true}),
+				// set: LogGroup.wrap(set, `[] Proxy set()`, (_, p, v) => `${p}=${v}`, {collapsed: true}),
+				get,
+				set,
 			},
 		) as WithHooks<T>;
 	}, 'State.create');
@@ -95,7 +97,7 @@ export namespace State {
 	export class Hook {
 		public handlers: Function[] = [];
 
-		@LogGroup('Hook.call()')
+		// @LogGroup('Hook.call()')
 		call(bound: Set<Context>, p?: any, v?: any) {
 			if (this.handlers.length) {
 				this.handlers[this.handlers.length - 1](bound, p, v);
